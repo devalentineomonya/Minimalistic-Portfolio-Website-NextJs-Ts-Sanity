cc972205f909f9fc27f520e8d426f96e1c8dd016
Valentine Omonya <valomosh254@gmail.com>
2025-03-24 23:58:42 +0300

    Updated projects component for improved styling

--- components/projects.tsx
+++ components/projects.tsx
@@ -1,46 +1,21 @@
+"use client";
+
 import React from "react";
 import { useInView } from "react-intersection-observer";
 import { ChevronRight } from "lucide-react";
 import ProjectCard from "./project-card";
 import Link from "next/link";
 import { motion } from "framer-motion";
+import { Projects } from "@/sanity/sanity.types";
 
-const ProjectsSection = () => {
+interface ProjectsSectionProps {
+  projects: Projects[];
+}
+const ProjectsSection: React.FC<ProjectsSectionProps> = ({ projects }) => {
   const { ref: projectsRef, inView: projectsInView } = useInView({
     triggerOnce: false,
     threshold: 0.1,
   });
-
-  const projects = [
-    {
-      id: "morva",
-      name: "Morva labs",
-      icon: "M",
-      iconBg: "#4f46e5",
-      description: "Visual design, Branding",
-      client: "Nur Pradiya",
-      company: "Morva labs",
-      projectType: "Visual design, Branding",
-      year: "2023",
-      fullDescription:
-        "Enhance your company's workforce management with a cutting-edge website. Enjoy seamless user experience, efficient employee management, and a competitive edge.",
-    },
-    {
-      id: "rectangle",
-      name: "Rectangle",
-      icon: "R",
-      iconBg: "#6366f1",
-      description: "Product design, Icon design",
-    },
-    {
-      id: "simply",
-      name: "Simply",
-      icon: "S",
-      iconBg: "#ef4444",
-      description: "Landing page, Illustration design",
-    },
-  ];
-
   return (
     <motion.section
       ref={projectsRef}
@@ -67,7 +42,7 @@ const ProjectsSection = () => {
       </div>
 
       <div className="space-y-3 mt-6">
-        {projects.map((project, index) => (
+        {projects?.map((project, index) => (
           <ProjectCard
             key={project.id}
             project={project}
