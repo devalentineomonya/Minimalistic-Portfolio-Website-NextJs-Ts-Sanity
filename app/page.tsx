191cfb163897d8482accb5c3cedf8dc622c86ba1
Valentine Omonya <valomosh254@gmail.com>
2025-03-24 23:58:33 +0300

    Refactored homepage layout and content

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,45 @@
-"use client";
-
-import { AnimatePresence } from "framer-motion";
 import Footer from "@/components/footer";
 import HeroSection from "@/screens/home/hero";
 import ProjectsSection from "@/components/projects";
 import ProductsSection from "@/components/products";
+import { defineQuery } from "next-sanity";
+import { sanityFetch } from "@/sanity/lib/live";
+import { Products, Projects } from "@/sanity/sanity.types";
 
-export default function HomePage() {
+export const PRODUCTS_QUERY = defineQuery(`
+      *[_type == "products"][0..3] |  order(_createdAt desc) {
+          id,
+          name,
+          icon,
+          iconBg,
+          category,
+          url,
+      }`);
+
+export const PROJECTS_QUERY = defineQuery(`
+            *[_type == "projects"][0..3] |  order(_createdAt desc) {
+                name,
+                description,
+                id,
+                iconBg,
+                icon
+            }`);
+
+export default async function HomePage() {
+  const { data: products } = (await sanityFetch({ query: PRODUCTS_QUERY })) as {
+    data: Products[];
+  };
+  const { data: projects } = (await sanityFetch({ query: PROJECTS_QUERY })) as {
+    data: Projects[];
+  };
   return (
-    <div className="min-h-[85vh] flex flex-col">
-      <AnimatePresence mode="wait">
-        <>
-          <HeroSection />
-          <ProjectsSection />
-          <ProductsSection />
-          <Footer />
-        </>
-      </AnimatePresence>
+    <div className="bg-background dark:bg-[#212121] rounded-lg  min-h-[85vh] flex flex-col">
+      <>
+        <HeroSection />
+        <ProjectsSection projects={projects} />
+        <ProductsSection products={products} />
+        <Footer />
+      </>
     </div>
   );
 }
